1679ca6ed37efae6b8ed01f8a2165184d7b6a1fb
Amelie <amelie.parkinson.bates@gmail.com>
2021-03-25 18:08:09 +1100

    Add project files.

---
+++ EasierSpecialOrders/ModConfig.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasierSpecialOrders
+{
+    internal class ModConfig
+    {
+        public bool RemoveCollectionObjective { get; set; } = true;
+        public QiChallenges EasierQiChallenges { get; set; } = new QiChallenges();       
+        internal class QiChallenges
+        {
+            public bool ShipLessQiFruit { get; set; } = false;
+            public bool DonateLessPrismaticShards { get; set; } = false;
+            public bool LowerJunimoKartScore { get; set; } = false;
+            public bool ShipLessCookedItems { get; set; } = false;
+            public bool GiveLessGifts { get; set; } = false;
+            public bool MoreTimeForExtendedFamily { get; set; } = false;
+            public bool LessItemsForQisPrismaticGrange { get; set; } = false;
+        }
+    }
+}
